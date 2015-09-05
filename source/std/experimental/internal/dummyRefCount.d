6bd80f99cb25a0e36c75eee6220b894d41e8fbef
Richard Cattermole <alphaglosined@gmail.com>
2015-09-06 02:05:22 +1200

    Moving auto deallocate out of VFS so PNG can take advantage of it

---
+++ source/std/experimental/internal/dummyRefCount.d
@@ -0,0 +1,16 @@
+﻿module std.experimental.internal.dummyRefCount;
+import std.experimental.allocator : IAllocator;
+
+/// When std.typecons.RefCounted adds support for allocators this will be replaced by it
+struct DummyRefCount(T) {
+    ///
+    T bytes;
+    alias bytes this;
+    
+    private IAllocator allocator;
+    
+    ~this() {
+        import std.experimental.allocator : dispose;
+        allocator.dispose(cast(T)bytes);
+    }
+}
