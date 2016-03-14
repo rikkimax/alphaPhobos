579c948f1eaa1c942049bdb5abd8f50914a5765d
rikki cattermole <pbdadmin@gmail.com>
2016-03-14 20:43:47 +1300

    General API improvements for notifications

--- source/std/experimental/ui/notifications.d
+++ source/std/experimental/ui/notifications.d
@@ -11,6 +11,7 @@ import std.experimental.graphic.color : RGBA8;
 import std.experimental.platform : IPlatform;
 import std.experimental.memory.managed;
 import std.experimental.allocator : IAllocator, theAllocator;
+import std.traits : isSomeString;
 
 interface Have_Notification {
     Feature_Notification __getFeatureNotification();
@@ -26,7 +27,14 @@ interface Feature_Notification {
     void clearNotifications();
 }
 
-///
+/**
+ * Assigns a notification icon to the given application
+ * 
+ * Params:
+ * 		self	=	The platform instance
+ * 		to		=	The image to assign as
+ * 		alloc	=	The allocator to use during assignment
+ */
 void notificationIcon(IPlatform self, ImageStorage!RGBA8 to, IAllocator alloc=theAllocator) {
     if (self is null)
         return;
@@ -38,7 +46,16 @@ void notificationIcon(IPlatform self, ImageStorage!RGBA8 to, IAllocator alloc=th
     }
 }
 
-///
+/**
+ * Retrieve the applications notification icon
+ * 
+ * Params:
+ * 		self	=	The platform instance
+ * 		alloc	=	The allocator to allocate/deallocate during creation
+ * 
+ * Returns:
+ * 		The notification icon for the current process (will auto deallocate)
+ */
 managed!(ImageStorage!RGBA8) notificationIcon(IPlatform self, IAllocator alloc=theAllocator) {
     if (self is null)
         return (managed!(ImageStorage!RGBA8)).init;
@@ -51,19 +68,67 @@ managed!(ImageStorage!RGBA8) notificationIcon(IPlatform self, IAllocator alloc=t
     return (managed!(ImageStorage!RGBA8)).init;
 }
 
-///
-void notify(IPlatform self, ImageStorage!RGBA8 image=null, dstring title=null, dstring text=null, IAllocator alloc=theAllocator) {
+/**
+ *  Sends a notification to the user for the current application, optionally setting an image and title
+ * 
+ * If the title and text is not a dstring it will automatically convert it to dstring and deallocate after usage.
+ * If the implementation needs to keep the value it around it will copy it.
+ * 
+ * Params:
+ * 		self	=	The platform instance
+ * 		image	=	An image to display along with the message
+ * 		title	=	A title to give the user
+ * 		text	=	The message text to tell the user
+ * 		alloc	=	Allocator to allocate and copy resources for while notification is active
+ */
+void notify(S1, S2)(IPlatform self, ImageStorage!RGBA8 image=null, S1 title=null, S2 text=null, IAllocator alloc=theAllocator) if (isSomeString!S1 && isSomeString!S2) {
     if (self is null)
         return;
     if (Have_Notification ss = cast(Have_Notification)self) {
-        auto fss = ss.__getFeatureNotification();
+		import std.utf : byDchar, codeLength;
+		import std.experimental.allocator : makeArray, dispose;
+
+		auto fss = ss.__getFeatureNotification();
+
+		dchar[] titleUse, textUse;
+
+		static if (is(S1 == dstring)) {
+			titleUse = cast()title;
+		} else {
+			titleUse = alloc.makeArray!dchar(codeLength!dstring(title));
+
+			foreach(i, c; title.byDchar) {
+				titleUse[i] = c;
+			}
+		}
+
+		static if (is(S2 == dstring)) {
+			textUse = cast()text;
+		} else {
+			textUse = alloc.makeArray!dchar(codeLength!dstring(text));
+			
+			foreach(i, c; text.byDchar) {
+				textUse[i] = c;
+			}
+		}
+
         if (fss !is null) {
-            fss.notify(image, title, text, alloc);
+			fss.notify(image, cast(dstring)titleUse, cast(dstring)textUse, alloc);
         }
+
+		static if (!is(S1 == dstring))
+			alloc.dispose(titleUse);
+		static if (!is(S2 == dstring))
+			alloc.dispose(textUse);
     }
 }
 
-///
+/**
+ * Forces deallocation and removal of all current and past (still stored) notifications
+ *
+ * Params:
+ * 		self	=	The platform instance
+ */
 void clearNotifications(IPlatform self) {
     if (self is null)
         return;
@@ -73,4 +138,22 @@ void clearNotifications(IPlatform self) {
             fss.clearNotifications();
         }
     }
+}
+
+/**
+ * Does the given platform support notifications for the user?
+ * 
+ * Params:
+ * 		self	=	The platform instance
+ * 
+ * Returns:
+ * 		If the platform supports notifications
+ */
+@property bool capableOfNotifications(IPlatform self) {
+	if (self is null)
+		return false;
+	else if (Have_Notification ss = cast(Have_Notification)self)
+		return ss.__getFeatureNotification() !is null;
+	else
+		return false;
 }
