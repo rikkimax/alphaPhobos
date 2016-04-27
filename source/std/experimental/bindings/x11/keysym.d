f57d0520fbb6e4f1075021b81178036f0ac9dce7
Rikki Cattermole <alphaglosined@gmail.com>
2016-04-28 03:34:47 +1200

    Finishing off keysym[def] bindings

---
+++ source/std/experimental/bindings/x11/keysym.d
@@ -0,0 +1,103 @@
+﻿/**
+Copyright 1987, 1998  The Open Group
+
+Permission to use, copy, modify, distribute, and sell this software and its
+documentation for any purpose is hereby granted without fee, provided that
+the above copyright notice appear in all copies and that both that
+copyright notice and this permission notice appear in supporting
+documentation.
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
+OPEN GROUP BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN
+AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+Except as contained in this notice, the name of The Open Group shall not be
+used in advertising or otherwise to promote the sale, use or other dealings
+in this Software without prior written authorization from The Open Group.
+
+
+Copyright 1987 by Digital Equipment Corporation, Maynard, Massachusetts.
+
+                        All Rights Reserved
+
+Permission to use, copy, modify, and distribute this software and its 
+documentation for any purpose and without fee is hereby granted, 
+provided that the above copyright notice appear in all copies and that
+both that copyright notice and this permission notice appear in 
+supporting documentation, and that the name of Digital not be
+used in advertising or publicity pertaining to distribution of the
+software without specific, written prior permission.  
+
+DIGITAL DISCLAIMS ALL WARRANTIES WITH REGARD TO THIS SOFTWARE, INCLUDING
+ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS, IN NO EVENT SHALL
+DIGITAL BE LIABLE FOR ANY SPECIAL, INDIRECT OR CONSEQUENTIAL DAMAGES OR
+ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS,
+WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION,
+ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS
+SOFTWARE.
+*/
+module std.experimental.bindings.x11.keysym;
+
+///
+enum XK_MISCELLANY = true;
+///
+enum XK_XKB_KEYS = true;
+///
+enum XK_LATIN1 = true;
+///
+enum XK_LATIN2 = true;
+///
+enum XK_LATIN3 = true;
+///
+enum XK_LATIN4 = true;
+///
+enum XK_LATIN8 = true;
+///
+enum XK_LATIN9 = true;
+///
+enum XK_CAUCASUS = true;
+///
+enum XK_GREEK = true;
+///
+enum XK_KATAKANA = true;
+///
+enum XK_ARABIC = true;
+///
+enum XK_CYRILLIC = true;
+///
+enum XK_HEBREW = true;
+///
+enum XK_THAI = true;
+///
+enum XK_KOREAN = true;
+///
+enum XK_ARMENIAN = true;
+///
+enum XK_GEORGIAN = true;
+///
+enum XK_VIETNAMESE = true;
+///
+enum XK_CURRENCY = true;
+///
+enum XK_MATHEMATICAL = true;
+///
+enum XK_BRAILLE = true;
+///
+enum XK_SINHALA = true;
+
+///
+enum XK_3270 = false;
+///
+enum XK_TECHNICAL = false;
+///
+enum XK_SPECIAL = false;
+///
+enum XK_PUBLISHING = false;
+///
+enum XK_APL = false;
--- source/std/experimental/bindings/x11/package.d
+++ source/std/experimental/bindings/x11/package.d
@@ -4,6 +4,8 @@ public import std.experimental.bindings.x11.Xlib;
 public import std.experimental.bindings.x11.Xmd;
 public import std.experimental.bindings.x11.Xproto;
 public import std.experimental.bindings.x11.Xprotostr;
+public import std.experimental.bindings.x11.keysym;
+public import std.experimental.bindings.x11.keysymdef;
 
 import std.experimental.bindings.autoloader;
 import std.experimental.bindings.symbolloader : SELF_SYMBOL_LOOKUP;
